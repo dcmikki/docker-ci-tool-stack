3291cc5130f214b94d30b8f78918304285ea96fa
dcmikki <dcmikki@gmail.com>
2018-12-31 00:47:24 +0000

    correction docker-compose command

--- setup.sh
+++ setup.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-#
-# Simply execute this script to setup the full toolchain:
-#
-# curl -sSL https://raw.githubusercontent.com/dcmikki/docker-ci-tool-stack/master/setup.sh | bash -s
-#
-# Prerequisites:
-# - Docker & Docker Toolbox v1.10
-# - Git v2.6.4
 
 echo "Create docker machine"
 docker-machine create -d virtualbox --virtualbox-memory "6000" docker-ci-tools
@@ -20,4 +12,4 @@ cd docker-ci-tool-stack
 git clone https://github.com/sonatype/docker-nexus3.git
 
 echo "Startup Docker Compose"
-docker-compose up
+docker-compose -f docker-compose.yml up
